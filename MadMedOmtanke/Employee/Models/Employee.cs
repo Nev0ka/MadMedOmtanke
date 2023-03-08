dbb8325d7f25916519814a4c2f4040774c3501c0
casp103a <casp103a@edu.sde.dk>
2023-03-08 12:45:59 +0100

    Added Models in a class libary.

---
+++ MadMedOmtanke/Employee/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Models
+{
+    internal class Employee
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string TlfNr { get; set; }
+        public string FirmEmail { get; set; }
+        public string Position { get; set; }
+        public int DepartmentID { get; set; }
+        public string ClosetManager { get; set; }
+        public string Skills { get; set; }
+    }
+}
