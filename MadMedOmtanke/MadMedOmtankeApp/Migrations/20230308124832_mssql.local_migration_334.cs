80dfc5ac5125923cba37d41f33fadc1f64aeb830
casp103a <casp103a@edu.sde.dk>
2023-03-08 13:49:55 +0100

    Fixed The Local database on my PC.

---
+++ MadMedOmtanke/MadMedOmtankeApp/Migrations/20230308124832_mssql.local_migration_334.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MadMedOmtankeApp.Migrations
+{
+    public partial class mssqllocal_migration_334 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Initials = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TlfNr = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirmEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PositionID = table.Column<int>(type: "int", nullable: false),
+                    DepartmentID = table.Column<int>(type: "int", nullable: false),
+                    ClosetManager = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Skills = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
